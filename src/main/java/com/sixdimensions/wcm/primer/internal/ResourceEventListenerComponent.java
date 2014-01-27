3c01c6fd097067dd71ee9f57d5759d326cddc572
Dan Klco <dan.klco@sixdimensions.com>
2014-01-27 16:22:54 -0500

    Fixing to unregister service when complete

--- src/main/java/com/sixdimensions/wcm/primer/internal/ResourceEventListenerComponent.java
+++ src/main/java/com/sixdimensions/wcm/primer/internal/ResourceEventListenerComponent.java
@@ -4,6 +4,7 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
@@ -14,20 +15,24 @@ public class ResourceEventListenerComponent {
 	private static final Logger log = LoggerFactory
 			.getLogger(ResourceEventListenerComponent.class);
 
+	private ServiceRegistration registration;
+	
+
 	protected void registerEventLisener(BundleContext bundleContext) {
 		String[] topics = new String[] { "org/apache/sling/api/resource/Resource/*" };
 
+		EventHandler handler = new EventHandler() {
+			public void handleEvent(Event event) {
+				String title = (String) event.getProperty("title");
+				String path = (String) event.getProperty("path");
+				log.info("Title: {} Path: {}", new Object[] { title,
+						path });
+			}
+		};
 		Dictionary<Object, Object[]> props = new Hashtable<Object, Object[]>();
 		props.put(EventConstants.EVENT_TOPIC, topics);
-		bundleContext.registerService(EventHandler.class.getName(),
-				new EventHandler() {
-					public void handleEvent(Event event) {
-						String title = (String) event.getProperty("title");
-						String path = (String) event.getProperty("path");
-						log.info("Title: {} Path: {}", new Object[] { title,
-								path });
-					}
-				}, props);
+		registration = bundleContext.registerService(EventHandler.class.getName(),
+				handler, props);
 	}
 
 }
