7ea68e7f318d466c383f9b9bc7d9279785629daa
Dan Klco <dan.klco@sixdimensions.com>
2014-01-27 15:39:22 -0500

    Adding the sample component

---
+++ src/main/java/com/sixdimensions/wcm/primer/internal/ResourceEventListenerComponent.java
@@ -0,0 +1,33 @@
+package com.sixdimensions.wcm.primer.internal;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResourceEventListenerComponent {
+	private static final Logger log = LoggerFactory
+			.getLogger(ResourceEventListenerComponent.class);
+
+	protected void registerEventLisener(BundleContext bundleContext) {
+		String[] topics = new String[] { "org/apache/sling/api/resource/Resource/*" };
+
+		Dictionary<Object, Object[]> props = new Hashtable<Object, Object[]>();
+		props.put(EventConstants.EVENT_TOPIC, topics);
+		bundleContext.registerService(EventHandler.class.getName(),
+				new EventHandler() {
+					public void handleEvent(Event event) {
+						String title = (String) event.getProperty("title");
+						String path = (String) event.getProperty("path");
+						log.info("Title: {} Path: {}", new Object[] { title,
+								path });
+					}
+				}, props);
+	}
+
+}
